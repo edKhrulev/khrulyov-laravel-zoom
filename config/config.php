16defa34a5762bbb400599911daad1e67b0e570b
Eduard Khrulyov <edKhrulev@gmail.com>
2023-03-30 18:52:31 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,8 +6,8 @@ return [
     'account_id' => env('ZOOM_ACCOUNT_ID'),
     'base_url' => 'https://api.zoom.us/v2/',
     'oauth_token_url' => 'https://zoom.us/oauth/token',
-    'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
+    'token_life' => 60 * 60 * 24 * 7, 
     'authentication_method' => env('ZOOM_AUTH_METHOD', 'jwt'), 
-    'max_api_calls_per_request' => '5' // how many times can we hit the api to return results for an all() request
-    'token_json'                => env('ZOOM_AUTH_TOKEN_JSON'),
+    'max_api_calls_per_request' => '5',
+    'token_json' => env('ZOOM_AUTH_TOKEN_JSON'),
 ];
